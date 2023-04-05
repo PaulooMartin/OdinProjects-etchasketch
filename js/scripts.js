478a2de785b326ce7f49ef1059efe4a1a9811a05
Paul Martin <paulmartinblopez@gmail.com>
2023-04-05 08:50:33 +0800

    Changes to ui and variable names

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,14 @@ const htmlBody = document.querySelector('body');
 const gridSection = document.createElement("section");
 const sketchArea = document.createElement('div');
 
-let gridSquares = [];
-let cell = [];
+let gridRows = [];
+let gridCells = [];
+let rows = 16;
+let columns = 16;
+
 
 createGrid(16,16)
-gridSquares.forEach(element => sketchArea.appendChild(element));
+gridRows.forEach(element => sketchArea.appendChild(element));
 
 gridSection.appendChild(sketchArea);
 htmlBody.appendChild(gridSection);
@@ -15,10 +18,10 @@ function createGrid(rows, columns){
     let cellNum = 0;
 
     for (i = 0; i < rows; i++){
-        gridSquares[i] = document.createElement('div');
+        gridRows[i] = document.createElement('div');
         for (j = 0; j < columns; j++){
-            cell[cellNum] = document.createElement('div');
-            gridSquares[i].appendChild (cell[cellNum]);
+            gridCells[cellNum] = document.createElement('div');
+            gridRows[i].appendChild (gridCells[cellNum]);
             cellNum++;
         }
     }
